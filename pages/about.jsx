457a6c813fc21fa9342ce072df338407af0679ff
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2021-08-27 12:47:15 +0430

    Add some arbitrary routes

---
+++ pages/about.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint accusamus
+        ipsam voluptas provident, dolorem optio itaque nesciunt similique
+        adipisci. Voluptates fugit voluptate alias, beatae eos magni inventore
+        unde ipsum incidunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint accusamus
+        ipsam voluptas provident, dolorem optio itaque nesciunt similique
+        adipisci. Voluptates fugit voluptate alias, beatae eos magni inventore
+        unde ipsum incidunt?
+      </p>
+    </div>
+  );
+};
+
+export default About;
