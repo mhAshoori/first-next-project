a890dd3726c59370d09788ba0ef51dca3edfd1ea
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2021-08-27 12:46:27 +0430

    initailize next-js & modify root index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint accusamus
+        ipsam voluptas provident, dolorem optio itaque nesciunt similique
+        adipisci. Voluptates fugit voluptate alias, beatae eos magni inventore
+        unde ipsum incidunt?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint accusamus
+        ipsam voluptas provident, dolorem optio itaque nesciunt similique
+        adipisci. Voluptates fugit voluptate alias, beatae eos magni inventore
+        unde ipsum incidunt?
+      </p>
+    </div>
+  );
+}
