bebb6bfd629f89eee42eded90c536af910d4164e
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2021-08-27 13:01:07 +0430

    Add "Navbar" & "Footer"

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            Copy Right 2021
+        </div>
+    )
+}
+
+export default Footer
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
 export default function Home() {
   return (
     <div >
+      <Navbar />
       <h1>Home</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint accusamus
@@ -18,6 +20,7 @@ export default function Home() {
         adipisci. Voluptates fugit voluptate alias, beatae eos magni inventore
         unde ipsum incidunt?
       </p>
+      <Footer/>
     </div>
   );
 }
